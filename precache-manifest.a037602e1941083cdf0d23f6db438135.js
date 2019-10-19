9459b545fcd33b998c0c23f6bee41626e5ec6f3c
anlex N <1293006794@qq.com>
2019-10-19 16:47:57 +0800

    add alipay for computer website

---
+++ precache-manifest.a037602e1941083cdf0d23f6db438135.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a5cbcc6c0b4951e0bbef2b230ce05978",
+    "url": "./index.html"
+  },
+  {
+    "revision": "5b2db2cf55b66f34b106",
+    "url": "css/app.css"
+  },
+  {
+    "revision": "9ee96e3a07dec6235d3f1a86787a92b7",
+    "url": "fonts/Framework7Icons-Regular.eot"
+  },
+  {
+    "revision": "3b973a05c0e1544f7ee8fa23c2a9315a",
+    "url": "fonts/Framework7Icons-Regular.ttf"
+  },
+  {
+    "revision": "535bcf7dd3feb41ec20507c3c5b81efb",
+    "url": "fonts/Framework7Icons-Regular.woff"
+  },
+  {
+    "revision": "1dfb73e22b9119f547fb880568f56ea2",
+    "url": "fonts/Framework7Icons-Regular.woff2"
+  },
+  {
+    "revision": "e79bfd88537def476913f3ed52f4f4b3",
+    "url": "fonts/MaterialIcons-Regular.eot"
+  },
+  {
+    "revision": "a37b0c01c0baf1888ca812cc0508f6e2",
+    "url": "fonts/MaterialIcons-Regular.ttf"
+  },
+  {
+    "revision": "012cf6a10129e2275d79d6adac7f3b02",
+    "url": "fonts/MaterialIcons-Regular.woff"
+  },
+  {
+    "revision": "570eb83859dc23dd0eec423a49e147fe",
+    "url": "fonts/MaterialIcons-Regular.woff2"
+  },
+  {
+    "revision": "5b2db2cf55b66f34b106",
+    "url": "js/app.js"
+  },
+  {
+    "revision": "da3f4eb773b07fe7214a4050c64e9a6c",
+    "url": "manifest.json"
+  },
+  {
+    "revision": "328b63e09aed08a4b289547150b469a8",
+    "url": "static/icons/128x128.png"
+  },
+  {
+    "revision": "7d0d3ecd607bfa50d566eaeed2a42075",
+    "url": "static/icons/144x144.png"
+  },
+  {
+    "revision": "5eb04b906f17c58a86609744904fc5fc",
+    "url": "static/icons/152x152.png"
+  },
+  {
+    "revision": "3fb2524920269b3b6b5d0f356f924b0b",
+    "url": "static/icons/196x196.png"
+  },
+  {
+    "revision": "0f03b546c382bb3ea68d2ba8153ff528",
+    "url": "static/icons/256x256.png"
+  },
+  {
+    "revision": "8205cfcf41c2697bbf16399ff8e91fa7",
+    "url": "static/icons/512x512.png"
+  },
+  {
+    "revision": "0f03b546c382bb3ea68d2ba8153ff528",
+    "url": "static/icons/apple-touch-icon.png"
+  },
+  {
+    "revision": "328b63e09aed08a4b289547150b469a8",
+    "url": "static/icons/favicon.png"
+  }
+]);
