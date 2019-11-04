499471b07c95a0f4517fa6f759ab8c3ad4a53815
anlex N <1293006794@qq.com>
2019-11-04 22:38:41 +0800

    jiujiuya

---
+++ precache-manifest.da018db7f9450fcac915ac42723695fd.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2c48ba33a70e83247090",
+    "url": "/css/app.409806c0.css"
+  },
+  {
+    "revision": "dfac54e94741c7b12a018636fadff6eb",
+    "url": "/img/180x180.dfac54e9.jpg"
+  },
+  {
+    "revision": "d677d4a0663811d4e7f6e9941176af4f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f8533b45a0bee6c39a0f",
+    "url": "/js/about.d040d0ef.js"
+  },
+  {
+    "revision": "2c48ba33a70e83247090",
+    "url": "/js/app.d503b6ba.js"
+  },
+  {
+    "revision": "728bcb68c649b5bee404",
+    "url": "/js/chunk-vendors.e8d05494.js"
+  },
+  {
+    "revision": "cdf2980dfb06988f644d67e09bcf6854",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
